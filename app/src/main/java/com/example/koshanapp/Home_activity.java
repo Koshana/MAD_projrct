6619199a2b7f9b4b1bc0457b5e0f1cf8a655d985
Dulmini <chuta@1234>
2019-09-18 00:20:46 +0530

    new one

--- app/src/main/java/com/example/koshanapp/Home_activity.java
+++ app/src/main/java/com/example/koshanapp/Home_activity.java
@@ -2,13 +2,96 @@ package com.example.koshanapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home_activity extends AppCompatActivity {
+
+    private Button button;
+    private Button button1;
+    private Button button2;
+    private Button button4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_activity);
+
+
+        button=(Button) findViewById(R.id.login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Start_homepage();
+            }
+        });
+
+        button2=(Button) findViewById(R.id.RegiGuid);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Start_Guid_page();
+            }
+        });
+
+
+
+        button1=(Button)findViewById(R.id.RegiUser);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                start_User_registratin();
+            }
+        });
+
+        button4=(Button) findViewById(R.id.ResetPassword);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Start_resetPassword_page();
+            }
+        });
+
+
+
     }
+
+
+    public void Start_homepage(){
+
+        Intent i =new Intent(this,NewsFeed.class);
+        startActivity(i);
+
+    }
+
+
+    public void start_User_registratin(){
+
+        Intent i =new Intent(this,User_registration.class);
+        startActivity(i);
+
+    }
+
+    public void Start_Guid_page(){
+
+        Intent i =new Intent(this,Guid_Registration.class);
+        startActivity(i);
+
+    }
+
+    public void Start_resetPassword_page(){
+
+        Intent i =new Intent(this,Reset_Password.class);
+        startActivity(i);
+
+    }
+
+
 }
--- app/src/main/java/com/example/koshanapp/MainActivity.java
+++ app/src/main/java/com/example/koshanapp/MainActivity.java
@@ -4,96 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
-    private Button button1;
-    private Button button2;
-    private Button button4;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tx=(TextView) findViewById(R.id.ed);
+        //Typeface customerfont = Typeface.createFromAsset(getAssets(),"Sweet Hipster.ttf");
+        //tx.setTypeface(customerfont);
 
 
-        button=(Button) findViewById(R.id.login);
-        button.setOnClickListener(new View.OnClickListener() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Home_activity.class);
+                startActivity(intent);
+                finish();
 
-                Start_homepage();
             }
-        });
-
-        button2=(Button) findViewById(R.id.RegiGuid);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Start_Guid_page();
-            }
-        });
-
-
-
-        button1=(Button)findViewById(R.id.RegiUser);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                start_User_registratin();
-            }
-        });
-
-        button4=(Button) findViewById(R.id.ResetPassword);
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Start_resetPassword_page();
-            }
-        });
-
-
+        },2500);
 
     }
-
-
-    public void Start_homepage(){
-
-        Intent i =new Intent(this,NewsFeed.class);
-        startActivity(i);
-
-    }
-
-
-    public void start_User_registratin(){
-
-        Intent i =new Intent(this,User_registration.class);
-        startActivity(i);
-
-    }
-
-    public void Start_Guid_page(){
-
-        Intent i =new Intent(this,Guid_Registration.class);
-        startActivity(i);
-
-    }
-
-    public void Start_resetPassword_page(){
-
-        Intent i =new Intent(this,Reset_Password.class);
-        startActivity(i);
-
-    }
-
-
 
 
 }
