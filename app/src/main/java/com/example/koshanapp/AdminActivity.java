e92d3899a714782709d4eb06009a312bcf0186ce
kaniya24 <dkanishka24@gmail.com>
2019-09-27 02:35:19 +0530

    Finalized Guiders Part. But Have A Small Issue :(

--- app/src/main/java/com/example/koshanapp/AdminActivity.java
+++ app/src/main/java/com/example/koshanapp/AdminActivity.java
@@ -48,7 +48,6 @@ public class AdminActivity extends AppCompatActivity {
                             DeleteGuiders();
                         }
 
-
                     }
                 });
     }
